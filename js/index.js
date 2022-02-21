2291a12a8d137ba80c410d3e0ab3d16812e21cf7
Shidul Islam <sishahin093@gmail.com>
2022-02-21 21:43:10 +0600

    33-2 Intro to JSON, JSON Structure, parse, stringify

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log('connection');
+
+// javascript objects 
+const user = { id: 11, name: 'shidul islam', job: 'actor' };
+const stringified = JSON.stringify(user);
+console.log(user);
+console.log(stringified);
+
+const shop = {
+    name: 'alia store',
+    address: 'jhulon pol',
+    propits: 150000,
+    products: ['laptop', 'mobile', 'pepsi'],
+    isExpensive: false
+};
+const shopStringify = JSON.stringify(shop);
+// console.log(shop);
+console.log(shopStringify);
+console.log(JSON.parse(shopStringify));
